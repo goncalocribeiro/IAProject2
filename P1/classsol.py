10c90a29c79f16ff0d03bc3aff305475151387b1
Gonçalo Ribeiro <goncalocribeiro@tecnico.ulisboa.pt>
2017-11-28 23:44:44 +0000

    just comments

--- P1/classsol.py
+++ P1/classsol.py
@@ -6,29 +6,37 @@ import timeit
 
 from sklearn.model_selection import cross_val_score
 
+# 4.1 Escolha de Features (1 valor)
+# numero de letras
+# numero de vogais
+
 def features(X):
     
     F = np.zeros((len(X),5))
     for x in range(0,len(X)):
         F[x,0] = len(X[x])
-        F[x,1] = 
-        F[x,2] = 
-        F[x,3] = 
-        F[x,4] = 
+        F[x,1] = #implementar ???
+        F[x,2] = #implementar ???
+        F[x,3] = #implementar ???
+        F[x,4] = #implementar ???
 
-    return F     
+    return F   
 
+# 4.2 Metodo de Aprendizagem (2 valores)
 def mytraining(f,Y):
-    
+    clf = #implementar
+    clf = clf.fit(f, Y)
    
     return clf
     
 def mytrainingaux(f,Y,par):
+	clf = #implementar
+    clf = clf.fit(f, Y)
     
     return clf
 
 def myprediction(f, clf):
-    Ypred = clf.predict(f)
+    Ypred = clf.predict(f) # os metodos no scikit learn tem todos uma função predict
 
     return Ypred
 
